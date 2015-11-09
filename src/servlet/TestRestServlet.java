7d86347dae356fafa53b6574c16752ea0da07507
affmarcos <mp.azeredo@hotmail.com>
2015-11-09 19:13:24 -0200

    Limpando código testrest

--- src/servlet/TestRestServlet.java
+++ src/servlet/TestRestServlet.java
@@ -22,47 +22,14 @@ public class TestRestServlet extends HttpServlet {
 	
 	private static final long serialVersionUID = 1L;
 
-	/*	protected String montaStringCategorias(){
-			ArrayList<Categorias> categorias = new Categorias().getCategorias();
-			String categoria="";
-			int atual=1;
-			if(categorias.size()>0){
-				categoria = categorias.get(0).getUrl();
-				while(atual<categorias.size()){
-					categoria= categoria+"||"+categorias.get(atual).getUrl();
-					atual++;
-				}
-			}
-			return categoria;
-		}
-		
-		protected String montaStringFilmes(){
-			ArrayList<Filme> filmes = new Filme().getFilmes();
-			String filme="";
-			int atual=1;
-			if(filmes.size()>0){
-				filme = filmes.get(0).getUrl();
-				while(atual<filmes.size()){
-					filme= filme+"||"+filmes.get(atual).getUrl();
-					atual++;
-				}
-			}
-			return filme;
-		}
-	
-*/
- 
   protected void doGet(HttpServletRequest request, HttpServletResponse response)
       throws ServletException, IOException {
     String path = request.getPathInfo();
     path = path.substring(path.lastIndexOf("/")+1,path.length());
-    //System.out.println(path+" "+categoria.getCategorias());
-   // out.println(request.getPathInfo());
-    //out.println(request.getParameterMap());
     try {
     	Categorias cat = new Categorias();
     	Filme fil = new Filme();
-       	String filmeOuCategoria =  buscaUrl(path, path);
+       	String filmeOuCategoria =  buscaUrlFilmeCategoria(path, path);
        	if(filmeOuCategoria.equals("categoria")){
 	        request.setAttribute("nomeCategoriaAtual", (Object) cat.buscaNomeCategoria(path));
 	        RequestDispatcher rd = request.getRequestDispatcher("/filmePorcategoria.jsp" );
@@ -82,12 +49,12 @@ public class TestRestServlet extends HttpServlet {
   }
   
   
-  protected String buscaUrl( String path, String nome) throws ServletException{
+  protected String buscaUrlFilmeCategoria( String path, String nome) throws ServletException{
 	  
 	  Categorias cat = new Categorias();
 	  Filme fil = new Filme();
 	  if(cat.buscaNomeCategoria(path).length()>0) return "categoria"; //categoria
-	  if(fil.buscaNomeFilme(path).length()>0) return "filme"; //categoria
+	  if(fil.buscaNomeFilme(path).length()>0) return "filme"; //filme
 
 	  throw new ServletException("Invalid URI");
   }
